279e47eae3ed0e2fbb1ec5e6b05f9c4e643ac6c2
lelign <90132981+lelign@users.noreply.github.com>
2025-10-27 09:00:30 +0300

    Update dd.sh thou web

--- dd.sh
+++ dd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #echo -e "\n"
+home=`pwd`
 echo -e "\n\tEnter full path to sd card image"
 read path_sd_card_image
 dev_exist=false
@@ -10,7 +11,7 @@ if [ -f $path_sd_card_image ]; then
 	        if [[ -n $(df | grep "/media/$(whoami)" | grep -o "/dev/$device[0-9]") ]]; then
         	        echo -e "\n\t Needs umount!"
                 	for dev in $(df | grep "/media/$(whoami)" | grep -o "/dev/$device[0-9]"); do
-                        umount $dev
+                        sudo umount $dev
 						echo -e "\t\tUmounted $dev"
                 	done
         	fi
@@ -20,6 +21,18 @@ if [ -f $path_sd_card_image ]; then
 		if [ $do_it == "y" ]; then
 			sudo dd if=$path_sd_card_image of=/dev/$device status=progress
 			sync
+			#echo -e "\n\t\t$path_sd_card_image"
+			echo ""
+			sudo fdisk -l | grep $device  | tee -a $home/log
+			cp $home/log $(dirname $path_sd_card_image)/used_files
+			root_path=$(sudo fdisk -l | grep sde | grep Linux | cut -d " " -f 1)
+			rm -rf tmp_dir && mkdir tmp_dir
+			sudo mount $root_path tmp_dir
+			cp $(dirname $path_sd_card_image)/used_files ./tmp_dir/home/root -r
+			if  [ -d "./tmp_dir/home/root/used_files" ]; then
+				echo -e "\n\t\t used files added to sd card on path /home/root/used_files"
+			fi
+			sudo umount $root_path
 		else
 			echo -e "...exit"
 			sleep 3
@@ -32,4 +45,5 @@ else
 	echo -e "\t\t\Path\n $path_sd_card_image\n\t\t\tNOT EXIST!!!"
 fi
 exit
+
 
